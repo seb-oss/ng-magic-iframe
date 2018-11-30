84e87c90e8d8829c283e851c81c0e4fc2aa1ecd9
Robert Hjalmers <robert.hjalmers@seb.se>
2018-11-30 17:20:20 +0100

    chore: clean up

--- projects/magic-iframe/src/app/app.module.ts
+++ projects/magic-iframe/src/app/app.module.ts
@@ -3,18 +3,16 @@ import { Injector, NgModule } from '@angular/core';
 import { NgMagicIframeModule } from '../../../ng-magic-iframe/src/lib/ng-magic-iframe.module';
 import { NgMagicIframeComponent } from '../../../ng-magic-iframe/src/lib/components/ng-magic-iframe.component';
 import { createCustomElement } from '@angular/elements';
-import { WebTestComponent } from './web-test/web-test.component';
 
 
 @NgModule({
-  declarations: [WebTestComponent],
+  declarations: [],
   imports: [
     BrowserModule,
     NgMagicIframeModule
   ],
   providers: [],
     entryComponents: [
-        WebTestComponent,
         NgMagicIframeComponent
     ]
 })
